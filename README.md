e8222d9e74d5efe926d02715581140396bb2c658
Shuhao Cao <scao.math@gmail.com>
2020-10-09 14:06:52 -0500

    Update hw.py

--- README.md
+++ README.md
@@ -6,4 +6,13 @@
 ## Primer on Python 3
 [https://www.kaggle.com/learn/python](https://www.kaggle.com/learn/python)
 
+## Structure of this repo
+```bash
+├── Homework
+│   ├── imports needed
+│  
+├── Lectures file
+├── Office hour files
+│   ├── codes during live stream
 
+```
---
+++ homework/hw2.py
@@ -0,0 +1,72 @@
+#%%
+import numpy as np
+from numpy.linalg import *
+import matplotlib.pyplot as plt
+from scipy.sparse import *
+Inf = np.inf
+
+# %%
+def getMatrix(n=10, isDiagDom=True):
+    '''
+    Return an nxn matrix which is the discretization matrix
+    from finite difference for a diffusion problem.
+    To visualize A: use plt.spy(A.toarray())
+    '''
+    # Representation of sparse matrix and right-hand side
+    assert n >= 2
+    n -= 1
+    diagonal  = np.zeros(n+1)
+    lower     = np.zeros(n)
+    upper     = np.zeros(n)
+
+    # Precompute sparse matrix
+    if isDiagDom:
+        diagonal[:] = 1 + 2/n
+    else:
+        diagonal[:] = 2/n
+    lower[:] = -1/n  #1
+    upper[:] = -1/n  #1
+    # Insert boundary conditions
+    diagonal[0] = 1
+    upper[0] = 0
+    diagonal[n] = 1
+    lower[-1] = 0
+
+    
+    A = diags(
+        diagonals=[diagonal, lower, upper],
+        offsets=[0, -1, 1], shape=(n+1, n+1),
+        format='csr')
+
+    return A
+
+def getAuxMatrix(A):
+    '''
+    return D, L, U matrices for Jacobi or Gauss-Seidel
+    D: array
+    L, U: matrices
+    '''
+    m = A.shape[0]
+    D = csr_matrix.diagonal(A)
+    L = -tril(A, k=-1)
+    U = -triu(A, k=1)
+    return D, L, U
+
+def plotConvergence(x_true, x, k=2, scale='log'):
+    '''
+    Plot the error ||x[i] - x_true||_k against i
+    x_true: (n,) vector
+    x: (m, n) matrix, x[i] is the i-th iterate, m: total number of iterations
+    scale: 'log' or 'linear'
+    k: 1,2,..., of Inf
+    '''
+    numIter = x.shape[0]
+    error = np.zeros(numIter)
+    for i in range(numIter):
+        error[i] = norm(x_true - x[i], k)
+    if scale is 'log':
+        plt.semilogy(error)
+    else:
+        plt.plot(error)
+
+
