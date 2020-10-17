7a057ed4768e416223eb0a3c922746c6f9b242a6
Shuhao Cao <scao.math@gmail.com>
2020-10-16 22:53:55 -0500

    Updateing plotConvergence function

--- homework/hw2.py
+++ homework/hw2.py
@@ -52,23 +52,29 @@ def getAuxMatrix(A):
     U = -triu(A, k=1)
     return D, L, U
 
-def plotConvergence(x_true, x, k=2, scale='log'):
+def plotConvergence(x_true, x, k=2, scale='log', rate=True):
     '''
-    Plot the error ||x[i] - x_true||_k against i
+    Plot the error ||x[i] - x_true||_k against i or the rate of the convergence
     x_true: (n,) vector
     x: (m, n) matrix, x[i] is the i-th iterate, 
     x[0] = x0 is the initial guess
     m: total number of iterations
     scale: 'log' or 'linear'
     k: 1,2,..., of Inf
+    rate: bool (optional) If true, plot the rate of the convergence
+    ||x[i+1] - x_true||_k/||x[i] - x_true||_k against
     '''
     numIter = x.shape[0]
     error = np.zeros(numIter)
     for i in range(numIter):
         error[i] = norm(x_true - x[i], k)
-    if scale is 'log':
+    if (scale is 'log') and (not rate):
         plt.semilogy(error)
-    else:
+    elif (scale is 'linear') and (not rate):
         plt.plot(error)
+    elif (scale is 'log') and rate:
+        plt.plot(np.log(error[1:]/error[:-1]))
+    elif (scale is 'linear') and rate:
+        plt.plot(error[1:]/error[:-1])
 
 
